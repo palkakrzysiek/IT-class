dd9b89ddef22c5fb19df6d8529b383f379fb8eaf
Krzysztof Palka <palkakrzysiek@gmail.com>
2012-12-02 21:01:00 +0100

    jukebox

--- c/learning/head-first/string/jukebox.c
+++ c/learning/head-first/string/jukebox.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char const* argv[]) {
+void find_track(char*);
+
+int main()
+{
+    char search_for[80];
+    printf("Search for: ");
+    fgets(search_for, 80, stdin);
+    find_track(search_for); 
+    return 0;
+}
+
+
+void find_track(char search_for[])
+{
     char tracks[][80] = {
         "I left my heart in Harvard Med School",
         "Newark, Newark - a wonderful town",
@@ -9,5 +22,9 @@ int main(int argc, char const* argv[]) {
         "From here to maternity",
         "The girl from Iwo Jima",
     };
-    printf("%d\n", strlen(tracks[3]));
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (strstr(tracks[i], search_for))
+            printf("Track %i: '%s'\n", i, tracks[i]);
+    }
 }
