281914728a1f24845874f2afa4e38ba7b3af67b5
Krzysztof Palka <palkakrzysiek@gmail.com>
2013-01-16 22:35:28 +0100

    working upheap and downheap

--- algorithms/tasks/k_smallest_heap.cpp
+++ algorithms/tasks/k_smallest_heap.cpp
@@ -1,11 +1,11 @@
-inline int leftChild(int n){
-    return n+1;
-}
-inline int rightChild(int n){
-    return n+2;
-}
-inline int parent(int n){
-    return (n-1) / 2;
+#include <iostream>
+using namespace std;
+
+void printarray(double *A, int n){
+    for(int i = 0; i < n; i++){
+        cout << A[i] << ' ';
+    }
+    cout << endl;
 }
 
 void exchange(double *T, int x, int y){
@@ -14,22 +14,45 @@ void exchange(double *T, int x, int y){
     T[y] = temp;
 }
 
-void unheap (double *T, int N, int i){
-    int current = N;
-    T[current] = i;
-    while (current>0 && T[parent(current)] < T[current]) {
-        exchange(T, parent(current), current);
-        current = parent(current);
+void upheap(double *T, int N, int i){
+    if(i <= 0)
+        return;
+    int parent = (i-1)/2;
+    if (T[parent] > T[i]){
+        exchange(T, parent, i);
+        upheap(T, N, parent);
     }
+
 }
 
 void downheap (double *T, int N, int i){
-    exchange (T, N, 0);
-    N--;
-    int current = 0;
-    while (leftChild(current) <= N || leftChild(current) <= N) {
-        if(T[current] > T[leftChild(current)]){
-            //TODO
-        }
+    int rightChild = i*2 + 2;
+    int leftChild = i*2 +1;
+    int smallest;
+
+    if(leftChild < N && T[leftChild] < T[i]){
+        smallest = leftChild;
     }
+    else{
+        smallest = i;
+    }
+    if(rightChild < N && T[rightChild] < T[smallest]){
+        smallest = rightChild;
+    }
+    if(smallest != i){
+        exchange(T, smallest, i);
+        downheap(T, N, smallest);
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    double A[] = {4, 5, 3, 8, 9, 2, 1, 0};
+    int size = 8;
+    printarray(A, size);
+    downheap(A, size, 0);
+    upheap(A, size, 7);
+    printarray(A, size);
+
+    return 0;
 }
