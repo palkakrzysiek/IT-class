2eae90f524823a9033288134a3d684bf51f83ad4
Krzysiek Palka <palkakrzysiek@gmail.com>
2013-05-10 12:42:45 +0200

    lab4.sh ver 1.0

--- labs/os/lab4.sh
+++ labs/os/lab4.sh
@@ -1,43 +1,56 @@
 #!/bin/bash
 
-VERSION="0.1"
-HELP="Usage: $0 user's_file opponent's_file [username]\n\
-        -h - show this message\n\
-        -v - version of program"
+NORMAL="\033[m"
+BOLD="\033[1m"
+ITALIC="\033[4m"
+TERMINALSIZE=`stty size | awk '{ print $2 }'`
+
+VERSION="1.0"
+AUTHOR="Krzysztof Palka, IT, 181427"
+DATE="May 10, 2013"
+HELP="Usage:\
+
+ ${BOLD}${0}${NORMAL} ${ITALIC}user's_file${NORMAL} ${ITALIC}opponent's_file${NORMAL} [${ITALIC}username${NORMAL}];\
+ ${BOLD}${0}${NORMAL} [${ITALIC}OPTIONS${NORMAL}] $NORMAL
+    OPTIONS:
+        -h - show this message
+        -v - version of program
+Author: $AUTHOR"
 
 
 USERNAME=$USER
+OPPONENTNAME="Opponent"
 
 for ((i=1;i<=$#;i++)); do
-    if [ "${!i}" == "-v" ]; then
-        echo "$VERSION"
+    if [[ "${!i}" == "-v" || "${!i}" == "--version" ]]; then
+        echo -e "Version: $VERSION\nAuthor: $AUTHOR\nCreated: $DATE"
         exit 0
     fi
-    if [ "${!i}" == "-h" ]; then
+    if [[ "${!i}" == "-h" || "${!i}" == "--help" ]]; then
         echo -e "$HELP"
         exit 0
     fi
-    if [ $i == 1 ]; then
+    if [[ $i == 1 ]]; then
         USERFILEPATH=${!i}
     fi
-    if [ $i == 2 ]; then
+    if [[ $i == 2 ]]; then
         OPPONETFILEPATH=${!i}
     fi
-    if [ $i == 3 ]; then 
+    if [[ $i == 3 ]]; then 
         if [[ ! ${!i} =~ [\ \t]+ ]]; then
             USERNAME=${!i}
         fi
     fi
 done
 
-if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+if [[ $# -lt 2 ||  $# -gt 3 ]]; then
     echo -e "$HELP" >&2
     exit 1
 fi
 
 echo "[`date`] $USERFILEPATH $OPPONETFILEPATH $USERNAME" >> log.txt
 
-< /dev/zero head -c 0 > $USERFILEPATH
+echo -n "" > $USERFILEPATH
 chmod 644 $USERFILEPATH
 if [ ! -f $USERFILEPATH ]; then
     echo "[`date`] It's impossible to create file $USERFILEPATH" >&2 >> log.txt
@@ -45,7 +58,7 @@ if [ ! -f $USERFILEPATH ]; then
 fi
 
 i=0
-while [ ! -f $OPPONETFILEPATH ] || [ -s $OPPONETFILEPATH ]; do 
+while [[ ! -f $OPPONETFILEPATH  ||  -s $OPPONETFILEPATH ]]; do 
     clear
     echo "Your file has been successfully created. Waiting for your opponent. Try: $(( i += 1 ))"
     sleep 1
@@ -58,26 +71,41 @@ OPPONENTPOINTS="0"
 USERMOVES="0"
 OPPONENTMOVES="0"
 
-while [ $USERMOVES -le $OPPONENTMOVES ] && $CONTINUEGAME; do 
+while [[ $USERMOVES -le $OPPONENTMOVES ]] && $CONTINUEGAME; do 
 clear
-echo "$USERMOVES $OPPONENTMOVES"
+echo -e "\
+SCORE: $USERNAME: ${BOLD}${USERPOINTS}${NORMAL} | $OPPONENTNAME: ${BOLD}${OPPONENTPOINTS}${NORMAL}
+NUMBER OF TURNS: ${BOLD}${USERMOVES}${NORMAL}"
+for ((i=1;i<=${TERMINALSIZE};i++)); do
+    echo -ne "="
+done
+echo -e "
+Type \
+${ITALIC}paper${NORMAL}, \
+${ITALIC}rock${NORMAL}, \
+${ITALIC}scissors${NORMAL} or \
+${ITALIC}exit${NORMAL} "
+
+
     while true; do
         read LASTMOVE
-        echo $LASTMOVE
         case $LASTMOVE in
             rock)
                 echo "$USERNAME rock" >> $USERFILEPATH
                 USERMOVES=$((USERMOVES+1))
+                echo -n "Waiting for opponent" 
                 break
                 ;;
             paper)
                 echo "$USERNAME paper" >> $USERFILEPATH
                 USERMOVES=$((USERMOVES+1))
+                echo -n "Waiting for opponent" 
                 break
                 ;;
             scissors)
                 echo "$USERNAME scissors" >> $USERFILEPATH
                 USERMOVES=$((USERMOVES+1))
+                echo -n "Waiting for opponent" 
                 break
                 ;; 
             exit)
@@ -86,20 +114,63 @@ echo "$USERMOVES $OPPONENTMOVES"
                 break 
                 ;;
             *)
+                echo "Wrong choice, try again:"
                 continue 
                 ;;
         esac
     done
-    while [ $OPPONENTMOVES -ne $USERMOVES ]; do
+
+    while [[ $OPPONENTMOVES -ne $USERMOVES ]]; do
         echo -n "."
         OPPONENTMOVES=`wc -l $OPPONETFILEPATH | awk '{print $1 }'`
-        ((OPPONENTMOVES--))
         sleep 1
     done
+    echo ""
+    OPPONENTNAME=`tail -n 1 $OPPONETFILEPATH | awk '{ print $1 }'`
     USERCHOISE=`tail -n 1 $USERFILEPATH | awk '{ print $2 }'` 
     OPPONENTCHOISE=`tail -n 1 $OPPONETFILEPATH | awk '{ print $2 }'`
+
+    if [[ $USERCHOISE == "done" || $OPPONENTCHOISE == "done" ]]; then
+        echo "The game has been finished"
+        break
+    fi
+
     echo "$USERCHOISE vs $OPPONENTCHOISE"
 
-done
-    
+    if [[ $USERCHOISE == $OPPONENTCHOISE ]]; then
+        echo "draw"
+    fi
 
+    if [[ $USERCHOISE == "paper" && $OPPONENTCHOISE == "rock" ]]; then
+        echo "You win"
+        ((USERPOINTS++))
+    fi
+
+    if [[ $USERCHOISE == "paper" && $OPPONENTCHOISE == "scissors" ]]; then
+        echo "You lose"
+        ((OPPONENTPOINTS++))
+    fi
+
+    if [[ $USERCHOISE == "rock" && $OPPONENTCHOISE == "scissors" ]]; then
+        echo "You win"
+        ((USERPOINTS++))
+    fi
+
+    if [[ $USERCHOISE == "rock" && $OPPONENTCHOISE == "paper" ]]; then
+        echo "You lose"
+        ((OPPONENTPOINTS++))
+    fi
+
+    if [[ $USERCHOISE == "scissors" && $OPPONENTCHOISE == "paper" ]]; then
+        echo "You win"
+        ((USERPOINTS++))
+    fi
+
+    if [[ $USERCHOISE == "scissors" && $OPPONENTCHOISE == "rock" ]]; then
+        echo "You lose"
+        ((OPPONENTPOINTS++))
+    fi
+
+    read -p "press enter to continue"
+
+done
